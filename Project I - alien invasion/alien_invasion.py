ffd8a00e7c6febd9cab9155aaa389fcc9b2dca23
AneraSS <anera.svarc@gmail.com>
2022-12-03 22:50:46 +0100

    updated

--- Project I - alien invasion/alien_invasion.py
+++ Project I - alien invasion/alien_invasion.py
@@ -1,9 +1,9 @@
 import sys
-
 import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 
 class AlienInvasion:
@@ -22,11 +22,14 @@ class AlienInvasion:
 
         self.ship = Ship(self)
 
+        self.bullets = pygame.sprite.Group()        # class that behaves lik a list with extra functionalities
+
     def run_game(self):
         """Start the main loop of the game."""
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
             self._update_screen()
 
     def _check_events(self):
@@ -47,6 +50,9 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:               # press 'q' to quit
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
+
 
     def _check_keyup_events (self,event):
         """Respond to key released up."""
@@ -55,10 +61,17 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets group."""
+        new_bullet = Bullet(self)   # zašto nije prazno? jer je klasa?
+        self.bullets.add(new_bullet)
+
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
 
         pygame.display.flip()
 
--- Project I - alien invasion/settings.py
+++ Project I - alien invasion/settings.py
@@ -9,6 +9,11 @@ class Settings:
         self.screen_height = 400
         self.bg_color = (147, 215, 231)
 
-
         # Ship's settings
         self.ship_speed = 1
+
+        # Bullet settings
+        self.bullet_speed = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60,60,60)
